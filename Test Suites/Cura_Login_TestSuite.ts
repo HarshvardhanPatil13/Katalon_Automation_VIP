814a20bd84392604599f86611e0badeeb7abf77e
admin <admin@DESKTOP-RL2L5EI>
2024-01-13 19:14:36 +0530

    Added Cura Test Case

---
+++ Test Suites/Cura_Login_TestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Cura_Login_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>720ad0ad-28b0-40d6-bb9d-5bd8f6f29eb6</testSuiteGuid>
+   <testCaseLink>
+      <guid>98437f34-0a5d-47f3-9ba6-f0cb183d6b42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cura_Login_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
